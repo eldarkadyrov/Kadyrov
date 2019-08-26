facb0c7ffc7fd52c7affbe2f39e138ec3c181973
Eldar Kadyrov <kadyrov.eldar@outlook.com>
2019-08-26 09:01:37 +0600

    Added

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число а");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор");
+            string c = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+
+
+            if (c == "+")
+            {
+                Console.WriteLine("Результат равен");
+                Console.WriteLine(a + b);
+            }
+            if (c == "/")
+            {
+                Console.WriteLine("Результат равен");
+                Console.WriteLine(a / b);
+            }
+            if (c == "-")
+            {
+                Console.WriteLine("Результат равен");
+                Console.WriteLine(a - b);
+            }
+            if (c == "*")
+            {
+                Console.WriteLine("Результат равен");
+                Console.WriteLine(a * b);
+            }
+
+        }
+    }
+}
